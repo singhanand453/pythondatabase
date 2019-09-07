fdfac24bf2f07d4013d2deffbb7500ee5544f18b
Anand pratap singh <47928185+singhanand453@users.noreply.github.com>
2019-09-07 23:21:49 +0530

    Update time.py

--- time.py
+++ time.py
@@ -3,10 +3,10 @@ sender_mail = 'rockencena007@gmail.com'
 receivers_mail = ['sharmanaman0512@gmail.com']
 message = """From: From Person %s 
 To: To Person %s 
-Subject: sharma ji ka launda,,
+Subject:dvls,
 """%(sender_mail,receivers_mail)
 
 smtpObj = smtplib.SMTP("gamil.com",587)
 smtpObj.login("rockencena007@gmail.com","pass****")
 smtpObj.sendmail(sender_mail, receivers_mail, message)
-print("Successfully sent email")
+print("Successfully sent")
